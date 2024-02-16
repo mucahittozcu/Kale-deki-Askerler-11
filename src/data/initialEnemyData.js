10669a9ce3e5b72cfc82f2b3d8cfe4dbc7c925af
mucahittozcu <tozcumucahit@gmail.com>
2024-02-16 14:54:16 +0300

    Kale'deki Askerler

---
+++ src/data/initialEnemyData.js
@@ -0,0 +1,36 @@
+const enemyData1 = 
+  [
+    {
+      id: crypto.randomUUID(),
+      currentImage: '../public/images/enemy1A.png',
+      altImage: './images/enemy1B.png',
+      orientation: 'left',
+      position: {
+        top: 210,
+        left: 157,
+      },
+    },
+    {
+      id: crypto.randomUUID(),
+      currentImage: '../public/images/enemy2A.png',
+      altImage: './images/enemy2B.png',
+      orientation: 'right',
+      position: {
+        top: 105,
+        left: 187,
+      },
+    },
+    {
+      id: crypto.randomUUID(),
+      currentImage: '../public/images/enemy3A.png',
+      altImage: './images/enemy3B.png',
+      orientation: 'right',
+      position: {
+        top: 165,
+        left: 165,
+      },
+    },
+  ]
+
+  
+  export default enemyData1
